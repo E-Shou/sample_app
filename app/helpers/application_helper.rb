2ff875526f4641d01421d2c171f9ba6ed63fb188
EC2 Default User <ec2-user@ip-172-31-18-203.us-east-2.compute.internal>
2018-05-24 07:13:28 +0000

    Make a basic User model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,8 +9,7 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
-  
-    def bootstrap_class_for(flash_type)
+  def bootstrap_class_for(flash_type)
     case flash_type
       when :success
         "alert-success"
